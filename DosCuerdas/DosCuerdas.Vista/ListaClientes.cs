31b85e386f228ddd8055ca92b418b2d17f5c8237
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2023-04-13 20:31:37 -0600

    Cambios

--- DosCuerdas/DosCuerdas.Vista/ListaClientes.cs
+++ DosCuerdas/DosCuerdas.Vista/ListaClientes.cs
@@ -73,25 +73,25 @@ namespace DosCuerdas.Vista
                 dat_principal.DataSource = Negocios.Mostrar();
                 valorcelda = -1;
                 this.dat_principal.ReadOnly = true;
-                //NRoles NegociosRoles = new NRoles();
-                //List<EPermisos> perm = new List<EPermisos>();
-                //perm = NegociosRoles.llenar_Permisos(Id_Rol, "Loterias");
-                //if (perm.Where(x => x.Accion == "Agregar").FirstOrDefault() != null)
-                //{
-                this.btn_agregar.Enabled = true;
-                //}
-                //if (perm.Where(x => x.Accion == "Modificar").FirstOrDefault() != null)
-                //{
-                this.btn_editar.Enabled = true;
-                //}
-                //if (perm.Where(x => x.Accion == "Eliminar").FirstOrDefault() != null)
-                //{
-                this.btn_eliminar.Enabled = true;
-                //}
-                //if (perm.Where(x => x.Accion == "Consultar").FirstOrDefault() != null)
-                //{
-                this.btn_consultar.Enabled = true;
-                //}
+                RolesController NegociosRoles = new RolesController();
+                List<EPermisos> perm = new List<EPermisos>();
+                perm = NegociosRoles.llenar_Permisos(Id_Rol, "Clientes");
+                if (perm.Where(x => x.Accion == "Agregar").FirstOrDefault() != null)
+                {
+                    this.btn_agregar.Enabled = true;
+                }
+                if (perm.Where(x => x.Accion == "Modificar").FirstOrDefault() != null)
+                {
+                    this.btn_editar.Enabled = true;
+                }
+                if (perm.Where(x => x.Accion == "Eliminar").FirstOrDefault() != null)
+                {
+                    this.btn_eliminar.Enabled = true;
+                }
+                if (perm.Where(x => x.Accion == "Consultar").FirstOrDefault() != null)
+                {
+                    this.btn_consultar.Enabled = true;
+                }
             }
             catch (Exception ex)
             {
